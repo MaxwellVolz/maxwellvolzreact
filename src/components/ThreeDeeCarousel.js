29ff56db916448a300d82b73c75306a589a2b270
Max Volz <maxwellvolz@gmail.com>
2018-04-26 13:11:27 -0700

    added scss; needs testing; 3d carousel incoming

---
+++ src/components/ThreeDeeCarousel.js
@@ -0,0 +1,16 @@
+import React, { PureComponent, Component } from 'react';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+
+export default class ThreeDeeCarousel extends PureComponent<PropType> {
+    render() {
+        return (
+            <div className="carousel">
+                <figure>
+                </figure>
+            </div>
+        );
+    }
+}
